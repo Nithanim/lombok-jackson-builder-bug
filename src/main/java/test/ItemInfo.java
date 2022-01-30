86d4e80b9bcf5a45d66b96ca4974d0ab77f0f0ea
Nithanim <git@nithanim.me>
2022-01-30 12:42:01 +0100

    Reduce example to minimum

--- src/main/java/test/ItemInfo.java
+++ src/main/java/test/ItemInfo.java
@@ -1,29 +1,21 @@
 package test;
 
-import java.util.Set;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver;
+import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase;
 
-import jdk.jfr.MemoryAddress;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
 public class ItemInfo {
-  String name;
-  String icon;
-  String description;
-  String chatLink;
-  @Builder.Default int level = -1;
-  @Builder.Default int vendorValue = -1;
-  @Builder.Default int defaultSkin = -1;
-  Set<String> flags;
-  String[] restrictions;
-
   @JsonProperty("myName")
   @JsonTypeInfo(
       use = JsonTypeInfo.Id.CUSTOM,
@@ -31,13 +23,39 @@ public class ItemInfo {
       property = "type",
       visible = true)
   @JsonTypeIdResolver(ItemTypeResolver.class)
-  @Deprecated
-  @JsonAlias("dsfsdf")
-  @MemoryAddress
-  Details details;
+  @JsonAlias("test")
+  String details;
 
-  @SuppressWarnings("unchecked")
-  public <T extends Details> T getDetails() {
-    return (T) details;
+  public static void main(String[] args) throws Exception {
+    Field field = ItemInfo.class.getDeclaredField("details");
+    Objects.requireNonNull(field.getAnnotation(JsonTypeInfo.class));
+    Objects.requireNonNull(field.getAnnotation(JsonProperty.class));
+    Objects.requireNonNull(field.getAnnotation(JsonTypeIdResolver.class));
+
+    Method method = ItemInfo.ItemInfoBuilder.class.getMethod("details", String.class);
+    Objects.requireNonNull(method.getAnnotation(JsonTypeInfo.class), "JsonTypeInfo not on builder");
+    Objects.requireNonNull(method.getAnnotation(JsonProperty.class), "JsonProperty not on builder");
+
+    // This fails
+    Objects.requireNonNull(
+        method.getAnnotation(JsonTypeIdResolver.class), "JsonTypeIdResolver not on builder");
+  }
+
+  public static class ItemTypeResolver extends TypeIdResolverBase {
+
+    @Override
+    public String idFromValue(Object o) {
+      return null;
+    }
+
+    @Override
+    public String idFromValueAndType(Object o, Class<?> aClass) {
+      return null;
+    }
+
+    @Override
+    public JsonTypeInfo.Id getMechanism() {
+      return null;
+    }
   }
 }
